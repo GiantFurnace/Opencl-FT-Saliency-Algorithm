3721134fd6a918f0015017d1b1c221ad0e607ea8
chenzhengqiang <642346572@qq.com>
2019-01-25 11:10:51 +0800

    add global work size and local work size

--- include/config.h
+++ include/config.h
@@ -11,14 +11,18 @@ namespace config
     /*
     @note:sample image's rows is 160, cols 1280
     */
-    static const int IMAGE_ROWS = 160;
-    static const int IMAGE_COLS = 1280;
+    static const int IMAGE_ROWS = 640;
+    static const int IMAGE_COLS = 640;
     static const int IMAGE_PIXELS = IMAGE_ROWS * IMAGE_COLS;
+
+    static const int CL_REDUCTION_GLOBAL_WORK_SIZE = 640;
+    static const int CL_REDUCTION_LOCAL_WORK_SIZE = 64;
+    static const int CL_REDUCTION_GROUPS = 640 / 64;
 
     /*
     @note:using 32 blocks to calculate image's sum with reduction algorithm
    */
-    static const int REDUCE_BLOCKS = 32;
+    static const int REDUCE_BLOCKS =  (640 / 64) * 4;
 }
 
 #endif
